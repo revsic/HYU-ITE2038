b07793e0b8ca4b1d208eab6e9edeb57828ff714a
revsic <revsic99@gmail.com>
2019-10-07 13:15:40 +0900

    disk manager: Add free_page_t

--- project2/include/disk_manager.h
+++ project2/include/disk_manager.h
@@ -5,18 +5,30 @@
 
 typedef uint64_t pagenum_t;
 
-struct keyvalue_t {
+struct pair_t {
     uint64_t key;       // 8
     uint8_t value[120]; // 128
 };
 
-struct page_t {
+struct page_header_t {
     pagenum_t parent_page_number;   // 8
     uint32_t is_leaf;               // 12
     uint32_t number_of_keys;        // 16
     uint8_t reserved[104];          // 120
     pagenum_t special_page_number;  // 128
-    struct keyvalue_t pairs[31];    // 4096
+};
+
+struct free_page_t {
+    pagenum_t next_page_number;     // 8
+    uint8_t not_used[120];          // 128
+};
+
+struct page_t {
+    union {
+        page_header_t page_header;
+        free_page_t free_page;
+    } header;                       // 128
+    struct pair_t pairs[31];        // 4096
 };
 
 pagenum_t file_alloc_page();
