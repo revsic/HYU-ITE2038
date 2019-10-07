f1d2d3c97ee33daae7a65a085f38d92290b6497e
revsic <revsic99@gmail.com>
2019-10-07 14:48:35 +0900

    disk manager: Decl some functions

--- project2/include/disk_manager.h
+++ project2/include/disk_manager.h
@@ -1,14 +1,27 @@
 #ifndef DISK_MANAGER_H
 #define DISK_MANAGER_H
 
+#include <stdio.h>
+
 #include "headers.h"
 
-pagenum_t file_alloc_page();
+struct file_manager_t {
+    FILE* fp;
+    file_header_t file_header;
+};
 
-void file_free_page(pagenum_t pagenum);
+int create_file(struct file_manager_t* manager);
 
-void file_read_page(pagenum_t pagenum, struct page_t* dest);
+int open_file(char* filename, struct file_manager_t* manager);
 
-void file_write_page(pagenum_t pagenum, const struct page_t* src);
+int close_file(struct file_manager_t* manager);
+
+pagenum_t create_page(struct file_manager_t* manager);
+
+int free_page(pagenum_t pagenum, struct file_manager_t* manager);
+
+int read_page(pagenum_t pagenum, struct file_manager_t* manager, struct page_t* dst);
+
+int write_page(pagenum_t pagenum, struct file_manager_t* manager, struct page_t* src);
 
 #endif
