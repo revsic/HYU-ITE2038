6d37862a88fa6bb6e54ef4e1ed6b884020fb55d0
revsic <revsic99@gmail.com>
2019-10-07 12:51:56 +0900

    disk manager: Add header

---
+++ project2/include/disk_manager.h
@@ -0,0 +1,20 @@
+#ifndef DISK_MANAGER_H
+#define DISK_MANAGER_H
+
+#include <stdint.h>
+
+typedef uint64_t pagenum_t;
+
+struct page_t {
+
+};
+
+pagenum_t file_alloc_page();
+
+void file_free_page(pagenum_t pagenum);
+
+void file_read_page(pagenum_t pagenum, struct page_t* dest);
+
+void file_write_page(pagenum_t pagenum, const struct page_t* src);
+
+#endif
