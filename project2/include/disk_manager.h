80628ef809fc88c08c4f5d0b2d613f1c698504df
revsic <revsic99@gmail.com>
2019-10-07 16:05:12 +0900

    disk manager: Add prefix

--- project2/include/disk_manager.h
+++ project2/include/disk_manager.h
@@ -5,23 +5,27 @@
 
 #include "headers.h"
 
+#define MAX_FILE_PATH 512
+
 struct file_manager_t {
     FILE* fp;
-    file_header_t file_header;
+    struct file_header_t file_header;
 };
 
-int create_file(struct file_manager_t* manager);
+int file_open(char* filename, struct file_manager_t* manager);
 
-int open_file(char* filename, struct file_manager_t* manager);
+int file_close(struct file_manager_t* manager);
 
-int close_file(struct file_manager_t* manager);
+pagenum_t page_create(struct file_manager_t* manager);
 
-pagenum_t create_page(struct file_manager_t* manager);
+int page_free(pagenum_t pagenum, struct file_manager_t* manager);
 
-int free_page(pagenum_t pagenum, struct file_manager_t* manager);
+int page_read(pagenum_t pagenum,
+              struct file_manager_t* manager,
+              struct page_t* dst);
 
-int read_page(pagenum_t pagenum, struct file_manager_t* manager, struct page_t* dst);
-
-int write_page(pagenum_t pagenum, struct file_manager_t* manager, struct page_t* src);
+int page_write(pagenum_t pagenum,
+               struct file_manager_t* manager,
+               struct page_t* src);
 
 #endif
