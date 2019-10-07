af424c863731e51ce09e010cd79948524d57cdea
revsic <revsic99@gmail.com>
2019-10-07 13:06:01 +0900

    disk manager: Add contents on struct

--- project2/include/disk_manager.h
+++ project2/include/disk_manager.h
@@ -5,8 +5,19 @@
 
 typedef uint64_t pagenum_t;
 
-struct page_t {
+struct keyvalue_t {
+    uint64_t key;
+    uint8_t value[120];
+};
 
+struct page_t {
+    pagenum_t parent_page_number;
+    uint32_t is_leaf;
+    uint32_t number_of_keys;
+    uint32_t flags;
+    uint8_t reserved[104];
+    pagenum_t special_page_number;
+    struct keyvalue_t pairs[31];
 };
 
 pagenum_t file_alloc_page();
