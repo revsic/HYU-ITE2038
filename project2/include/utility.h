ba9e1eef1d603a561c237260b47c375865ea69b4
revsic <revsic99@gmail.com>
2019-10-08 19:49:50 +0900

    util: Add content getter fom complex structure

--- project2/include/utility.h
+++ project2/include/utility.h
@@ -1,6 +1,16 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
+#include "utility.h"
+
 int max(int a, int b);
+
+struct page_header_t* page_header(struct page_t* page);
+
+struct free_page_t* free_page(struct page_t* page);
+
+struct record_t* records(struct page_t* page);
+
+struct internal_t* entries(struct page_t* page);
 
 #endif
--- project2/src/utility.c
+++ project2/src/utility.c
@@ -1,5 +1,21 @@
-#include "utility.h"
+#include "headers.h"
 
 int max(int a, int b) {
     return a > b ? a : b;
+}
+
+struct page_header_t* page_header(struct page_t* page) {
+    return &page->header.page_header;
+}
+
+struct free_page_t* free_page(struct page_t* page) {
+    return &page->header.free_page.header;
+}
+
+struct record_t* records(struct page_t* page) {
+    return page->content.records;
+}
+
+struct internal_t* entries(struct page_t* page) {
+    return page->content.entries;
 }
