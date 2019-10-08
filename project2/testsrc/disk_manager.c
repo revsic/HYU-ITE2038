21db8555bc98a05aaa1d349949828e2199cda13a
revsic <revsic99@gmail.com>
2019-10-08 13:30:02 +0900

    disk manager: Add suite

--- project2/testsrc/disk_manager.c
+++ project2/testsrc/disk_manager.c
@@ -1,5 +1,54 @@
 #include "test.h"
 
+TEST_SUITE(file_init, {
+
+})
+
+TEST_SUITE(file_open, {
+
+})
+
+TEST_SUITE(file_close, {
+
+})
+
+TEST_SUITE(last_pagenum, {
+
+})
+
+TEST_SUITE(last_pagenum_from_size, {
+
+})
+
+TEST_SUITE(page_create, {
+
+})
+
+TEST_SUITE(page_extend_free, {
+
+})
+
+TEST_SUITE(page_free, {
+
+})
+
+TEST_SUITE(page_read, {
+
+})
+
+TEST_SUITE(page_write, {
+
+})
+
 int disk_manager_test() {
-    return 1;
+    return file_init_test()
+        && file_open_test()
+        && file_close_test()
+        && last_pagenum_test()
+        && last_pagenum_from_size_test()
+        && page_create_test()
+        && page_extend_free_test()
+        && page_free_test()
+        && page_read_test()
+        && page_write_test();
 }
