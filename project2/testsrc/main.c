4fe1cdbe47f41ba36e7cd23d1f07ffbcab47b4c0
revsic <revsic99@gmail.com>
2019-10-08 14:02:22 +0900

    main: Rename integrate test to unit test

--- project2/testsrc/main.c
+++ project2/testsrc/main.c
@@ -1,11 +1,11 @@
 #include "test.h"
 
-TEST_SUITE(integrate, {
+TEST_SUITE(unit, {
     TEST(fileio_test());
     TEST(headers_test());
     TEST(disk_manager_test());
 })
 
 int main() {
-    integrate_test();
+    unit_test();
 }
