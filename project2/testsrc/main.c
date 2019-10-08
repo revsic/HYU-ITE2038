9324bca6b5a95aea69d31ed28a819d1b5ec3ea43
revsic <revsic99@gmail.com>
2019-10-08 13:37:23 +0900

    main: Add integrate test

--- project2/testsrc/main.c
+++ project2/testsrc/main.c
@@ -1,7 +1,11 @@
 #include "test.h"
 
-int main() {
+TEST_SUITE(integrate, {
     TEST(fileio_test());
     TEST(headers_test());
     TEST(disk_manager_test());
+})
+
+int main() {
+    integrate_test();
 }
