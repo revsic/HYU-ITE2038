ebd5656c3808da666258dacf557bd6ff2a95c7dd
revsic <revsic99@gmail.com>
2019-10-08 13:40:58 +0900

    headers: Test structure size test

--- project2/testsrc/headers.c
+++ project2/testsrc/headers.c
@@ -1,3 +1,13 @@
+#include "headers.h"
+#include "test.h"
+
+TEST_SUITE(structure_size, {
+    TEST(sizeof(struct padded_file_header_t) == PAGE_SIZE);
+    TEST(sizeof(struct page_header_t) == 128);
+    TEST(sizeof(struct padded_free_page_t) == 128);
+    TEST(sizeof(struct page_t) == PAGE_SIZE);
+})
+
 int headers_test() {
-    return 1;
+    return structure_size_test();
 }
