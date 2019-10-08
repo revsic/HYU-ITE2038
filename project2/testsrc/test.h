f1a6359302d20f0558a99672d88bb90e689a0ba7
revsic <revsic99@gmail.com>
2019-10-08 11:57:48 +0900

    test: Add newlines

--- project2/testsrc/test.h
+++ project2/testsrc/test.h
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
-#define TEST(expr) if(!(expr)) { printf("%s, line %d: err\n", __FILE__, __LINE__); return 0; }
+#define TEST(expr)                                      \
+if(!(expr)) {                                           \
+    printf("%s, line %d: err\n", __FILE__, __LINE__);   \
+    return 0;                                           \
+}
 
-#define TEST_SUITE(name, content) int name##_test() {  printf(#name " test\n"); content; return 1; }
+#define TEST_SUITE(name, content)       \
+int name##_test() {                     \
+    content;                            \
+    printf(#name " test success\n");    \
+    return 1;                           \
+}
 
 int fileio_test();
