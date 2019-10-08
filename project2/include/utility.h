b4774e0605a5fc179979fc9b7a4287a63b48bf40
revsic <revsic99@gmail.com>
2019-10-08 19:52:17 +0900

    utility: Make inline

--- project2/include/utility.h
+++ project2/include/utility.h
@@ -1,16 +1,26 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
-#include "utility.h"
+#include "headers.h"
 
-int max(int a, int b);
+inline int max(int a, int b) {
+    return a > b ? a : b;
+}
 
-struct page_header_t* page_header(struct page_t* page);
+inline struct page_header_t* page_header(struct page_t* page) {
+    return &page->header.page_header;
+}
 
-struct free_page_t* free_page(struct page_t* page);
+inline struct free_page_t* free_page(struct page_t* page) {
+    return &page->header.free_page.header;
+}
 
-struct record_t* records(struct page_t* page);
+inline struct record_t* records(struct page_t* page) {
+    return page->content.records;
+}
 
-struct internal_t* entries(struct page_t* page);
+inline struct internal_t* entries(struct page_t* page) {
+    return page->content.entries;
+}
 
 #endif
