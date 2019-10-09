88f674d761588bb697c0b13de8c3ab56a438ac74
revsic <revsic99@gmail.com>
2019-10-10 01:55:16 +0900

    bpt: Add disk header

--- project2/include/bpt.h
+++ project2/include/bpt.h
@@ -5,6 +5,7 @@
 // #define WINDOWS
 #include <stdio.h>
 
+#include "disk_manager.h"
 #include "headers.h"
 
 // global constant
@@ -40,20 +41,20 @@ int path_to_root(pagenum_t node, struct file_manager_t* manager);
 void print_leaves(struct file_manager_t* manager);
 void print_tree(struct file_manager_t* manager);
 
-pagenum_t find_leaf(key_t key, struct page_t* page, struct file_manager_t* manager);
-int find(key_t key, struct record_t* record, struct file_manager_t* manager);
-int find_range(key_t start,
-               key_t end,
+pagenum_t find_leaf(prikey_t key, struct page_t* page, struct file_manager_t* manager);
+int find(prikey_t key, struct record_t* record, struct file_manager_t* manager);
+int find_range(prikey_t start,
+               prikey_t end,
                struct record_t* retval,
                struct file_manager_t* manager);
 
-void find_and_print(key_t key, struct file_manager_t* manager); 
-void find_and_print_range(key_t range1, key_t range2, struct file_manager_t* manager);
+void find_and_print(prikey_t key, struct file_manager_t* manager); 
+void find_and_print_range(prikey_t range1, prikey_t range2, struct file_manager_t* manager);
 
 int cut(int length);
 
 // Insertion.
-int make_record(struct record_t* record, key_t key, int value);
+int make_record(struct record_t* record, prikey_t key, int value);
 pagenum_t make_node(struct file_manager_t* manager, uint32_t leaf);
 
 int get_left_index(struct page_t* parent, pagenum_t left);
@@ -72,35 +73,34 @@ pagenum_t insert_into_node_after_splitting(struct page_pair_t* old_node,
                                            struct file_manager_t* manager);
 
 pagenum_t insert_into_parent(struct page_pair_t* left,
-                             key_t key,
+                             prikey_t key,
                              struct page_pair_t* right,
                              struct file_manager_t* manager);
 pagenum_t insert_into_new_root(struct page_pair_t* left,
-                               key_t key,
+                               prikey_t key,
                                struct page_pair_t* right,
                                struct file_manager_t* manager);
 
 pagenum_t start_new_tree(int key,
                          struct record_t* pointer,
                          struct file_manager_t* manager);
-pagenum_t insert(pagenum_t root,
-                 key_t key,
+pagenum_t insert(prikey_t key,
                  int value,
                  struct file_manager_t* manager);
 
 // Deletion.
 
-int get_neighbor_index( node * n );
-node * adjust_root(node * root);
-node * coalesce_nodes(node * root, node * n, node * neighbor,
-                      int neighbor_index, int k_prime);
-node * redistribute_nodes(node * root, node * n, node * neighbor,
-                          int neighbor_index,
-        int k_prime_index, int k_prime);
-node * delete_entry( node * root, node * n, int key, void * pointer );
-node * delete( node * root, int key );
+// int get_neighbor_index( node * n );
+// node * adjust_root(node * root);
+// node * coalesce_nodes(node * root, node * n, node * neighbor,
+//                       int neighbor_index, int k_prime);
+// node * redistribute_nodes(node * root, node * n, node * neighbor,
+//                           int neighbor_index,
+//         int k_prime_index, int k_prime);
+// node * delete_entry( node * root, node * n, int key, void * pointer );
+// node * delete( node * root, int key );
 
-void destroy_tree_nodes(node * root);
-node * destroy_tree(node * root);
+// void destroy_tree_nodes(node * root);
+// node * destroy_tree(node * root);
 
 #endif /* __BPT_H__*/
