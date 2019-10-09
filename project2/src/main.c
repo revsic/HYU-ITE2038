187a5c14143a7a5b114bd6baffd11aed79592012
revsic <revsic99@gmail.com>
2019-10-10 01:54:57 +0900

    main: Fix to disk mode

--- project2/src/main.c
+++ project2/src/main.c
@@ -1,64 +1,34 @@
 #include "bpt.h"
+#include "disk_manager.h"
 
 // MAIN
 
-int main( int argc, char ** argv ) {
-
-    char * input_file;
-    FILE * fp;
-    node * root;
+int main(int argc, char ** argv) {
     int input, range2;
     char instruction;
-    char license_part;
+    struct file_manager_t manager;
+    file_open("datafile", &manager);
 
-    root = NULL;
-    verbose_output = false;
-
-    if (argc > 1) {
-        order = atoi(argv[1]);
-        if (order < MIN_ORDER || order > MAX_ORDER) {
-            fprintf(stderr, "Invalid order: %d .\n\n", order);
-            usage_3();
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    license_notice();
     usage_1();  
     usage_2();
-
-    if (argc > 2) {
-        input_file = argv[2];
-        fp = fopen(input_file, "r");
-        if (fp == NULL) {
-            perror("Failure  open input file.");
-            exit(EXIT_FAILURE);
-        }
-        while (!feof(fp)) {
-            fscanf(fp, "%d\n", &input);
-            root = insert(root, input, input);
-        }
-        fclose(fp);
-        print_tree(root);
-    }
 
     printf("> ");
     while (scanf("%c", &instruction) != EOF) {
         switch (instruction) {
-        case 'd':
-            scanf("%d", &input);
-            root = delete(root, input);
-            print_tree(root);
-            break;
+        // case 'd':
+        //     scanf("%d", &input);
+        //     root = delete(root, input);
+        //     print_tree(root);
+        //     break;
         case 'i':
             scanf("%d", &input);
-            root = insert(root, input, input);
-            print_tree(root);
+            insert(input, input, &manager);
+            print_tree(&manager);
             break;
         case 'f':
         case 'p':
             scanf("%d", &input);
-            find_and_print(root, input, instruction == 'p');
+            find_and_print(input, &manager);
             break;
         case 'r':
             scanf("%d %d", &input, &range2);
@@ -67,26 +37,23 @@ int main( int argc, char ** argv ) {
                 range2 = input;
                 input = tmp;
             }
-            find_and_print_range(root, input, range2, instruction == 'p');
+            find_and_print_range(input, range2, &manager);
             break;
         case 'l':
-            print_leaves(root);
+            print_leaves(&manager);
             break;
         case 'q':
             while (getchar() != (int)'\n');
-            return EXIT_SUCCESS;
+            return 0;
             break;
         case 't':
-            print_tree(root);
+            print_tree(&manager);
             break;
-        case 'v':
-            verbose_output = !verbose_output;
-            break;
-        case 'x':
-            if (root)
-                root = destroy_tree(root);
-            print_tree(root);
-            break;
+        // case 'x':
+        //     if (root)
+        //         root = destroy_tree(root);
+        //     print_tree(root);
+        //     break;
         default:
             usage_2();
             break;
@@ -96,5 +63,5 @@ int main( int argc, char ** argv ) {
     }
     printf("\n");
 
-    return EXIT_SUCCESS;
+    return 0;
 }
