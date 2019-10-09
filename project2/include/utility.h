4910d8ce75ef9b634174123200aa48e84573f18f
revsic <revsic99@gmail.com>
2019-10-10 01:54:38 +0900

    util: Make instantiation

--- project2/include/utility.h
+++ project2/include/utility.h
@@ -3,24 +3,14 @@
 
 #include "headers.h"
 
-inline int max(int a, int b) {
-    return a > b ? a : b;
-}
+int max(int a, int b);
 
-inline struct page_header_t* page_header(struct page_t* page) {
-    return &page->header.page_header;
-}
+struct page_header_t* page_header(struct page_t* page);
 
-inline struct free_page_t* free_page(struct page_t* page) {
-    return &page->header.free_page.header;
-}
+struct free_page_t* free_page(struct page_t* page);
 
-inline struct record_t* records(struct page_t* page) {
-    return page->content.records;
-}
+struct record_t* records(struct page_t* page);
 
-inline struct internal_t* entries(struct page_t* page) {
-    return page->content.entries;
-}
+struct internal_t* entries(struct page_t* page);
 
 #endif
