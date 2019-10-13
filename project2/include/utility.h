3dc92847990dcff9dafc0c29a7572d23c2319886
revsic <revsic99@gmail.com>
2019-10-13 16:53:07 +0900

    utility: Fix annotation style

--- project2/include/utility.h
+++ project2/include/utility.h
@@ -4,39 +4,29 @@
 #include "headers.h"
 
 /// Return maximum value.
-/// Args:
-///     a: int, integer value.
-///     b: int, integer value.
-/// Returns:
-///     int, maximum value between two arguments.
+/// \param a int, integer value.
+/// \param b int, integer value.
+/// \return int, maximum value between two arguments.
 int max(int a, int b);
 
 /// Return page header from page structure.
-/// Args:
-///     page: struct page_t*, page structure.
-/// Returns:
-///     struct page_header_t*, page header.
+/// \param page: struct page_t*, page structure.
+/// \return struct page_header_t*, page header.
 struct page_header_t* page_header(struct page_t* page);
 
 /// Return free page header from page structure.
-/// Args:
-///     page: struct page_t*, page structure.
-/// Returns:
-///     struct free_page_t*, free page header.
+/// \param page: struct page_t*, page structure.
+/// \return struct free_page_t*, free page header.
 struct free_page_t* free_page(struct page_t* page);
 
 /// Return records array from page structure.
-/// Args:
-///     page: struct page_t*, page structure.
-/// Returns:
-///     struct record_t*, page header.
+/// \param page: struct page_t*, page structure.
+/// \return struct record_t*, page header.
 struct record_t* records(struct page_t* page);
 
 /// Return entries array from page structure.
-/// Args:
-///     page: struct page_t*, page structure.
-/// Returns:
-///     struct internal_t*, page header.
+/// \param page: struct page_t*, page structure.
+/// \return struct internal_t*, page header.
 struct internal_t* entries(struct page_t* page);
 
 #endif
