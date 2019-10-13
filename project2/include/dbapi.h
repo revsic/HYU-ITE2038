83b8961c27caf99ccee5c802f74f9c2bf5d22ea8
revsic <revsic99@gmail.com>
2019-10-13 20:16:58 +0900

    dbapi: Add annotation

--- project2/include/dbapi.h
+++ project2/include/dbapi.h
@@ -3,34 +3,80 @@
 
 #include "disk_manager.h"
 
+// TYPE DEFINTION
+
+/// Vector class for file manager.
 struct file_vec_t {
-    int size;
-    int capacity;
-    struct file_manager_t* manager;
+    int size;                       /// size of the vector.
+    int capacity;                   /// capacity of the vector.
+    struct file_manager_t* manager; /// file manager array.
 };
 
+
+// GLOBAL VARIABLES
+
+/// Table id for operating db-prefix operations.
 extern int GLOBAL_TABLE_ID;
 
+/// Global file manager vector.
 extern struct file_vec_t GLOBAL_FILE_MANAGER;
 
+
+// PROCEDURES
+
+/// Initialize file manager vector.
+/// \param vec struct file_vec_t*, file manager vector.
+/// \return int, whether success to initialize or not.
 int file_vec_init(struct file_vec_t* vec);
 
+/// Deallocate file manager vector.
+/// \param vec struct file_vec_t*, file manager vector.
+/// \return int, whether success to deallocate or not.
 int file_vec_free(struct file_vec_t* vec);
 
+/// Expand capacity of file manager array.
+/// \param vec struct file_vec_t*, file manager vector.
+/// \return int, whether expansion success or not.
 int file_vec_expand(struct file_vec_t* vec);
 
+/// Append file manager to vector.
+/// \param vec struct file_vec_t*, file manager vector.
+/// \param manager struct file_manager_t*, file manager.
+/// \return int, whether success to append or not.
 int file_vec_append(struct file_vec_t* vec, struct file_manager_t* manager);
 
-int open_table(char* pathname);
+/// Open table and save file manager to global context.
+/// \param pathname const char*, path of the file.
+/// \return int, created table ID.
+int open_table(const char* pathname);
 
+/// Close table with given table ID.
+/// \param tid int, table ID.
+/// \return int, whether success to close file or not.
 int close_table(int tid);
 
+/// Get file manager from table ID.
+/// \param tableid int, table ID.
+/// \return struct file_manager_t*, file manager.
 struct file_manager_t* get_file_manager(int tableid);
 
+/// Insert key and value to the most recent table.
+/// \param key prikey_t, key.
+/// \param value char*, value data.
+/// \return int, whether success to insert data or not.
+/// if key is duplicated, return FAILURE and do not update value.
 int db_insert(int64_t key, char* value);
 
+/// Find key from most recent table and return value.
+/// \param key prikey_t, key.
+/// \param value char*, value data, nullable.
+/// \return int, whether success to find data or not.
+/// if key is not found, return FAILURE.
 int db_find(int64_t key, char* ret_val);
 
+/// Delete key from table.
+/// \param key prikey_t, key.
+/// \return int, whether success to find data or not.
 int db_delete(int64_t key);
 
 #endif
--- project2/src/dbapi.c
+++ project2/src/dbapi.c
@@ -11,6 +11,7 @@ int GLOBAL_TABLE_ID = -1;
 struct file_vec_t GLOBAL_FILE_MANAGER = { 0, 0, NULL };
 
 int file_vec_init(struct file_vec_t* vec) {
+    // initialize file vector with default capacity.
     vec->size = 0;
     vec->capacity = DEFAULT_FILE_VEC_CAPACITY;
     vec->manager = malloc(sizeof(struct file_vec_t) * DEFAULT_FILE_VEC_CAPACITY);
@@ -22,55 +23,55 @@ int file_vec_free(struct file_vec_t* vec) {
 }
 
 int file_vec_expand(struct file_vec_t* vec) {
+    // expand capacity as twice
     vec->capacity *= 2;
     struct file_manager_t* new_vec =
         malloc(sizeof(struct file_vec_t) * vec->capacity);
-    
+    // copy file managers
     memcpy(new_vec, vec->manager, sizeof(struct file_vec_t) * vec->size);
+    // update array
     vec->manager = new_vec;
-
     return SUCCESS;
 }
 
 int file_vec_append(struct file_vec_t* vec, struct file_manager_t* manager) {
+    // if vector is not initialized
     if (vec->manager == NULL) {
         CHECK_SUCCESS(file_vec_init(vec));
     }
-
+    // if capacity is exhausted
     if (vec->size >= vec->capacity) {
         CHECK_SUCCESS(file_vec_expand(vec));
     }
-    
+    // copy manager
     vec->manager[vec->size++] = *manager;
+    // make zero
     memset(manager, 0, sizeof(struct file_manager_t));
-
     return SUCCESS;
 }
 
-int open_table(char* pathname) {
+int open_table(const char* pathname) {
+    // open file
     struct file_manager_t manager;
     if (file_open(pathname, &manager) == FAILURE) {
         return -1;
     }
-
+    // append manager
     EXIT_ON_FAILURE(file_vec_append(&GLOBAL_FILE_MANAGER, &manager));
+    // update global table id
     GLOBAL_TABLE_ID = GLOBAL_FILE_MANAGER.size - 1;
     return GLOBAL_TABLE_ID;
 }
 
 int close_table(int tableid) {
-    int i;
-    struct file_vec_t* ptr = &GLOBAL_FILE_MANAGER;
+    // close file
     struct file_manager_t* res = get_file_manager(tableid);
     CHECK_SUCCESS(file_close(res));
-
-    for (i = tableid; i < ptr->size - 1; ++i) {
-        ptr->manager[i] = ptr->manager[i + 1];
-    }
     return SUCCESS;
 }
 
 struct file_manager_t* get_file_manager(int tableid) {
+    // get manager from global file manager
     if (0 <= tableid && tableid < GLOBAL_FILE_MANAGER.size) {
         return &GLOBAL_FILE_MANAGER.manager[tableid];
     }
