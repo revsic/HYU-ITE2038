77d29cf25b9893fe18b518e950f641abe83bf708
revsic <revsic99@gmail.com>
2019-10-22 22:44:12 +0900

    test: Implement release test

--- project3/testsrc/buffer_manager.c
+++ project3/testsrc/buffer_manager.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 
 #include "buffer_manager.h"
+#include "utility.h"
 #include "test.h"
 
 TEST_SUITE(check_ubuffer, {
@@ -183,7 +184,95 @@ TEST_SUITE(buffer_append_mru, {
 })
 
 TEST_SUITE(buffer_release, {
+    struct buffer_manager_t manager;
+    TEST_SUCCESS(buffer_manager_init(&manager, 5));
 
+    struct buffer_t* target = &manager.buffers[1];
+
+    struct table_t table;
+    TEST_SUCCESS(table_init(&table));
+    TEST_SUCCESS(table_load(&table, "testfile"));
+    TEST_SUCCESS(buffer_init(target, 1, &manager));
+    TEST_SUCCESS(buffer_load(target, &table, FILE_HEADER_PAGENUM));
+
+    struct page_uri_t uri;
+    uri.table_id = table.table_id;
+    uri.pagenum = FILE_HEADER_PAGENUM;
+
+    // case 1. not dirty
+    manager.num_buffer = 3;
+    manager.lru = 0;
+    manager.mru = 2;
+    manager.buffers[0].prev_use = -1;
+    manager.buffers[0].next_use = 1;
+    manager.buffers[1].prev_use = 0;
+    manager.buffers[1].next_use = 2;
+    manager.buffers[2].prev_use = 1;
+    manager.buffers[2].next_use = -1;
+
+    TEST_SUCCESS(buffer_release(target));
+    // linkage
+    TEST(manager.lru == 0);
+    TEST(manager.mru == 2);
+    TEST(manager.buffers[0].prev_use == -1);
+    TEST(manager.buffers[0].next_use == 2);
+    TEST(manager.buffers[2].prev_use == 0);
+    TEST(manager.buffers[2].next_use == -1);
+    // block info
+    TEST(manager.buffers[1].prev_use == -1);
+    TEST(manager.buffers[1].next_use == -1);
+    TEST(manager.buffers[1].block_idx == 1);
+    TEST(manager.buffers[1].manager == &manager);
+    TEST(manager.buffers[1].table == NULL);
+    // file check
+    TEST_SUCCESS(buffer_load(target, &table, FILE_HEADER_PAGENUM));
+    TEST(file_header(from_buffer(target))->free_page_number == 0);
+    TEST(file_header(from_buffer(target))->root_page_number == INVALID_PAGENUM);
+    TEST(file_header(from_buffer(target))->number_of_pages == 0);
+
+    // case 2. dirty
+    struct ubuffer_t ubuffer;
+    ubuffer.buf = target;
+    ubuffer.uri = uri;
+    BUFFER_WRITE(ubuffer, {
+        file_header(from_ubuffer(&ubuffer))->root_page_number = 1;
+        file_header(from_ubuffer(&ubuffer))->number_of_pages = 20;
+    })
+
+DBG(file_header(from_ubuffer(&ubuffer))->root_page_number)
+
+    manager.num_buffer = 3;
+    manager.lru = 0;
+    manager.mru = 2;
+    manager.buffers[0].prev_use = -1;
+    manager.buffers[0].next_use = 1;
+    manager.buffers[1].prev_use = 0;
+    manager.buffers[1].next_use = 2;
+    manager.buffers[2].prev_use = 1;
+    manager.buffers[2].next_use = -1;
+
+    TEST_SUCCESS(buffer_release(target));
+    // linkage
+    TEST(manager.lru == 0);
+    TEST(manager.mru == 2);
+    TEST(manager.buffers[0].prev_use == -1);
+    TEST(manager.buffers[0].next_use == 2);
+    TEST(manager.buffers[2].prev_use == 0);
+    TEST(manager.buffers[2].next_use == -1);
+    // block info
+    TEST(manager.buffers[1].prev_use == -1);
+    TEST(manager.buffers[1].next_use == -1);
+    TEST(manager.buffers[1].block_idx == 1);
+    TEST(manager.buffers[1].manager == &manager);
+    TEST(manager.buffers[1].table == NULL);
+    // file check
+    TEST_SUCCESS(buffer_load(target, &table, FILE_HEADER_PAGENUM));
+    TEST(file_header(from_buffer(target))->free_page_number == 0);
+DBG(file_header(from_buffer(target))->root_page_number)
+    TEST(file_header(from_buffer(target))->root_page_number == 1);
+    TEST(file_header(from_buffer(target))->number_of_pages == 20);
+
+    remove("testfile");
 })
 
 TEST_SUITE(buffer_start, {
