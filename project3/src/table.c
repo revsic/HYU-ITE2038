6a0c86755d52e1b049ec15e55ea655c31d6d7c2d
revsic <revsic99@gmail.com>
2019-10-25 23:16:33 +0900

    table: Porting apis from disk manager

--- project3/src/table.c
+++ project3/src/table.c
@@ -39,3 +39,19 @@ int table_release(struct table_t* table) {
     table->table_id = INVALID_TABLENUM;
     return file_close(&table->file_manager);
 }
+
+pagenum_t table_create_page(struct table_t* table) {
+    return page_create(&table->file_manager);
+}
+
+int table_free_page(struct table_t* table, pagenum_t pagenum) {
+    return page_free(pagenum, &table->file_manager);
+}
+
+int table_read_page(struct table_t* table, pagenum_t pagenum, struct page_t* dst) {
+    return page_read(pagenum, &table->file_manager, dst);
+}
+
+int table_write_page(struct table_t* table, pagenum_t pagenum, struct page_t* src) {
+    return page_write(pagenum, &table->file_manager, src);
+}
