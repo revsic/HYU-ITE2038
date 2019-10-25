60c9d5dc11671bb800865b76d328bbab2bd5035a
revsic <revsic99@gmail.com>
2019-10-26 05:30:44 +0900

    table: Move table over bpt

--- project3/include/table.h
+++ project3/include/table.h
@@ -2,21 +2,31 @@
 #define TABLE_H
 
 #include "disk_manager.h"
+#include "bpt.h"
 #include "headers.h"
 
 struct table_t {
-    tablenum_t table_id;
-    struct file_manager_t file_manager;
+    tablenum_t id;
+    struct bpt_t bpt;
+    struct file_manager_t file;
 };
-
-tablenum_t create_tablenum(const char* filename);
-
-tablenum_t rehash_tablenum(tablenum_t tablenum);
 
 int table_init(struct table_t* table);
 
-int table_load(struct table_t* table, const char* filename);
+int table_load(struct table_t* table,
+               const char* filename,
+               struct buffer_manager_t* buffers);
 
 int table_release(struct table_t* table);
+
+int table_find(struct table_t* table, prikey_t key, struct record_t* record);
+
+int table_find_range(struct table_t* table, prikey_t start, prikey_t end, struct record_vec_t* retval);
+
+int table_insert(struct table_t* table, prikey_t key, uint8_t* value, int value_size);
+
+int table_delete(struct table_t* table, prikey_t key);
+
+tablenum_t table_rehash(struct table_t* table, int update_id);
 
 #endif
--- project3/src/table.c
+++ project3/src/table.c
@@ -1,41 +1,55 @@
 #include "table.h"
 
-// sdbm
-tablenum_t create_tablenum(const char* filename) {
-    char c;
-    unsigned long hash = 0;
-    while ((c = *filename++)) {
-        if (c == '/' || c == '\\') {
-            hash = 0;
-            continue;
-        }
-        hash = c + (hash << 6) + (hash << 16) - hash;
-    }
-    return (tablenum_t)hash;
-}
-
-// sdbm
-tablenum_t rehash_tablenum(tablenum_t tablenum) {
-    int i;
-    unsigned long hash = 0;
-    uint8_t* bytes = (uint8_t*)&tablenum;
-    for (i = 0; i < sizeof(tablenum_t); ++i) {
-        hash = bytes[i] + (hash << 6) + (hash << 16) - hash;
-    }
-    return (tablenum_t)hash;
-}
-
 int table_init(struct table_t* table) {
-    table->table_id = INVALID_TABLENUM;
+    table->id = INVALID_TABLENUM;
     return SUCCESS;
 }
 
-int table_load(struct table_t* table, const char* filename) {
-    table->table_id = create_tablenum(filename);
-    return file_open(&table->file_manager, filename);
+int table_load(struct table_t* table,
+               const char* filename,
+               struct buffer_manager_t* buffers)
+{
+    CHECK_SUCCESS(file_open(&table->file, filename));
+    CHECK_SUCCESS(bpt_init(&table->bpt, &table->file, buffers));
+    table->id = table->file.id;
+    return SUCCESS;
 }
 
 int table_release(struct table_t* table) {
-    table->table_id = INVALID_TABLENUM;
-    return file_close(&table->file_manager);
+    table->id = INVALID_TABLENUM;
+    CHECK_SUCCESS(file_close(&table->file));
+    CHECK_SUCCESS(bpt_release(&table->bpt));
+    return SUCCESS;
+}
+
+int table_find(struct table_t* table, prikey_t key, struct record_t* record) {
+    return bpt_find(&table->bpt, key, record);
+}
+
+int table_find_range(struct table_t* table, prikey_t start, prikey_t end, struct record_vec_t* vec) {
+    return bpt_find_range(&table->bpt, start, end, vec);
+}
+
+int table_insert(struct table_t* table, prikey_t key, uint8_t* value, int value_size) {
+    return bpt_insert(&table->bpt, key, value, value_size);
+}
+
+int table_delete(struct table_t* table, prikey_t key) {
+    return bpt_delete(&table->bpt, key);
+}
+
+tablenum_t table_rehash(struct table_t* table, int update_id) {
+    int i;
+    unsigned long hash = 0;
+    uint8_t* bytes = (uint8_t*)&table->id;
+    for (i = 0; i < sizeof(tablenum_t); ++i) {
+        hash = bytes[i] + (hash << 6) + (hash << 16) - hash;
+    }
+
+    if (update_id) {
+        table->id = hash;
+        table->file.id = hash;
+    }
+
+    return hash;
 }
