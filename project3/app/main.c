d76e504ca06007a12cb2aeefb6d481be4f24290f
revsic <revsic99@gmail.com>
2019-11-03 00:18:39 +0900

    main: Fix to table manager

--- project3/app/main.c
+++ project3/app/main.c
@@ -13,13 +13,15 @@ int main(int argc, char ** argv) {
     // init_db(4);
     // tablenum_t table_id = open_table("datafile");
     struct record_t rec;
-    struct table_t table;
+    struct table_manager_t tables;
     struct buffer_manager_t buffers;
     buffer_manager_init(&buffers, 10);
-    table_load(&table, "datafile", &buffers);
+    table_manager_init(&tables, 10);
+    tablenum_t tid = table_manager_load(&tables, "datafile", &buffers);
+    struct table_t* table = table_manager_find(&tables, tid);
 
-    usage_1(&table.bpt);
-    usage_2(&table.bpt);
+    usage_1(&table->bpt);
+    usage_2(&table->bpt);
 
     printf("> ");
     while (scanf("%c", &instruction) != EOF) {
@@ -38,18 +40,18 @@ int main(int argc, char ** argv) {
             break;
         case 'd':
             scanf("%d", &input);
-            table_delete(&table, input);
-            print_tree(&table.bpt);
+            table_delete(table, input);
+            print_tree(&table->bpt);
             break;
         case 'i':
             scanf("%d", &input);
             snprintf(value, 100, "%d value", input);
-            table_insert(&table, input, value, strlen(value) + 1);
-            print_tree(&table.bpt);
+            table_insert(table, input, value, strlen(value) + 1);
+            print_tree(&table->bpt);
             break;
         case 'f':
             scanf("%d", &input);
-            if (table_find(&table, input, &rec) == SUCCESS) {
+            if (table_find(table, input, &rec) == SUCCESS) {
                 printf("Key: %d  Value: %s\n", input, rec.value);
             } else {
                 printf("not found\n");
@@ -62,26 +64,26 @@ int main(int argc, char ** argv) {
                 range2 = input;
                 input = tmp;
             }
-            find_and_print_range(&table.bpt, input, range2);
+            find_and_print_range(&table->bpt, input, range2);
             break;
         case 'l':
-            print_leaves(&table.bpt);
+            print_leaves(&table->bpt);
             break;
         case 'q':
             while (getchar() != (int)'\n');
             buffer_manager_shutdown(&buffers);
-            table_release(&table);
+            table_manager_release(&tables);
             return 0;
             break;
         case 't':
-            print_tree(&table.bpt);
+            print_tree(&table->bpt);
             break;
         case 'x':
-            destroy_tree(&table.bpt);
-            print_tree(&table.bpt);
+            destroy_tree(&table->bpt);
+            print_tree(&table->bpt);
             break;
         default:
-            usage_2(&table.bpt);
+            usage_2(&table->bpt);
             break;
         }
         while (getchar() != (int)'\n');
