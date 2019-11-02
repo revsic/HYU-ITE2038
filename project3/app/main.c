0cbde825f77d95e25e266e0443a10b636f44271d
revsic <revsic99@gmail.com>
2019-11-02 23:21:16 +0900

    main: Run based on table

--- project3/app/main.c
+++ project3/app/main.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include "utility.h"
 
 #include "bpt.h"
 #include "dbapi.h"
@@ -13,15 +14,13 @@ int main(int argc, char ** argv) {
     // init_db(4);
     // tablenum_t table_id = open_table("datafile");
     struct record_t rec;
-    struct bpt_t bpt;
+    struct table_t table;
     struct buffer_manager_t buffers;
-    struct file_manager_t file;
-    file_open(&file, "datafile");
     buffer_manager_init(&buffers, 10);
-    bpt_init(&bpt, &file, &buffers);
+    table_load(&table, "datafile", &buffers);
 
-    usage_1(&bpt);
-    usage_2(&bpt);
+    usage_1(&table.bpt);
+    usage_2(&table.bpt);
 
     printf("> ");
     while (scanf("%c", &instruction) != EOF) {
@@ -40,21 +39,21 @@ int main(int argc, char ** argv) {
             break;
         case 'd':
             scanf("%d", &input);
-            bpt_delete(&bpt, input);
-            print_tree(&bpt);
+            table_delete(&table, input);
+            print_tree(&table.bpt);
             // db_delete(table_id, input);
             break;
         case 'i':
             scanf("%d", &input);
             snprintf(value, 100, "%d value", input);
-            bpt_insert(&bpt, input, value, strlen(value) + 1);
-            print_tree(&bpt);
+            table_insert(&table, input, value, strlen(value) + 1);
+            print_tree(&table.bpt);
             // db_insert(table_id, input, value);bpt);
             break;
         case 'f':
             scanf("%d", &input);
             // if (db_find(table_id, input, value) == SUCCESS) {
-            if (bpt_find(&bpt, input, &rec) == SUCCESS) {
+            if (table_find(&table, input, &rec) == SUCCESS) {
                 printf("Key: %d  Value: %s\n", input, rec.value);
             } else {
                 printf("not found\n");
@@ -67,28 +66,27 @@ int main(int argc, char ** argv) {
                 range2 = input;
                 input = tmp;
             }
-            find_and_print_range(&bpt, input, range2);
+            find_and_print_range(&table.bpt, input, range2);
             break;
         case 'l':
-            print_leaves(&bpt);
+            print_leaves(&table.bpt);
             break;
         case 'q':
             while (getchar() != (int)'\n');
             // shutdown_db();
-            bpt_release(&bpt);
             buffer_manager_shutdown(&buffers);
-            file_close(&file);
+            table_release(&table);
             return 0;
             break;
         case 't':
-            print_tree(&bpt);
+            print_tree(&table.bpt);
             break;
         case 'x':
-            destroy_tree(&bpt);
-            print_tree(&bpt);
+            destroy_tree(&table.bpt);
+            print_tree(&table.bpt);
             break;
         default:
-            usage_2(&bpt);
+            usage_2(&table.bpt);
             break;
         }
         while (getchar() != (int)'\n');
