8ae8bad465d0fa5b83015e047f987c6065f13c0a
revsic <revsic99@gmail.com>
2019-11-02 22:52:55 +0900

    main: Run on bpt based

--- project3/app/main.c
+++ project3/app/main.c
@@ -1,3 +1,5 @@
+#include <string.h>
+
 #include "bpt.h"
 #include "dbapi.h"
 
@@ -8,43 +10,52 @@ int main(int argc, char ** argv) {
     char instruction;
     char value[1024];
 
-    init_db(4);
-    tablenum_t table_id = open_table("datafile");
-    struct bpt_t* bpt = &table_manager_find(&GLOBAL_DBMS.tables, table_id)->bpt;
+    // init_db(4);
+    // tablenum_t table_id = open_table("datafile");
+    struct record_t rec;
+    struct bpt_t bpt;
+    struct buffer_manager_t buffers;
+    struct file_manager_t file;
+    file_open(&file, "datafile");
+    buffer_manager_init(&buffers, 10);
+    bpt_init(&bpt, &file, &buffers);
 
-    usage_1(bpt);
-    usage_2(bpt);
+    usage_1(&bpt);
+    usage_2(&bpt);
 
     printf("> ");
     while (scanf("%c", &instruction) != EOF) {
         switch (instruction) {
         case 'o':
             // open table
-            scanf("%1023s", value);
-            input = open_table(value);
+            // scanf("%1023s", value);
+            // input = open_table(value);
             // table id validation
-            if (input == -1) {
-                printf("cannot open file %s\n", value);
-            } else {
-                close_table(table_id);
-                table_id = input;
-            }
+            // if (input == -1) {
+            //     printf("cannot open file %s\n", value);
+            // } else {
+            //     close_table(table_id);
+            //     table_id = input;
+            // }
             break;
         case 'd':
             scanf("%d", &input);
-            db_delete(table_id, input);
-            print_tree(bpt);
+            bpt_delete(&bpt, input);
+            print_tree(&bpt);
+            // db_delete(table_id, input);
             break;
         case 'i':
             scanf("%d", &input);
             snprintf(value, 100, "%d value", input);
-            db_insert(table_id, input, value);
-            print_tree(bpt);
+            bpt_insert(&bpt, input, value, strlen(value) + 1);
+            print_tree(&bpt);
+            // db_insert(table_id, input, value);bpt);
             break;
         case 'f':
             scanf("%d", &input);
-            if (db_find(table_id, input, value) == SUCCESS) {
-                printf("Key: %d  Value: %s\n", input, value);
+            // if (db_find(table_id, input, value) == SUCCESS) {
+            if (bpt_find(&bpt, input, &rec) == SUCCESS) {
+                printf("Key: %d  Value: %s\n", input, rec.value);
             } else {
                 printf("not found\n");
             }
@@ -56,25 +67,28 @@ int main(int argc, char ** argv) {
                 range2 = input;
                 input = tmp;
             }
-            find_and_print_range(bpt, input, range2);
+            find_and_print_range(&bpt, input, range2);
             break;
         case 'l':
-            print_leaves(bpt);
+            print_leaves(&bpt);
             break;
         case 'q':
             while (getchar() != (int)'\n');
-            shutdown_db();
+            // shutdown_db();
+            bpt_release(&bpt);
+            buffer_manager_shutdown(&buffers);
+            file_close(&file);
             return 0;
             break;
         case 't':
-            print_tree(bpt);
+            print_tree(&bpt);
             break;
         case 'x':
-            destroy_tree(bpt);
-            print_tree(bpt);
+            destroy_tree(&bpt);
+            print_tree(&bpt);
             break;
         default:
-            usage_2(bpt);
+            usage_2(&bpt);
             break;
         }
         while (getchar() != (int)'\n');
