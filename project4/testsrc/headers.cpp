27bc8b5ad47d5a3fddde6026e4b66b4ec9d8697c
revsic <revsic99@gmail.com>
2019-11-05 14:03:10 +0900

    headers: Add headers test

--- project4/testsrc/headers.cpp
+++ project4/testsrc/headers.cpp
@@ -1,5 +1,13 @@
 #include "test.hpp"
 
-TEST_SUITE(headers, {
+TEST_SUITE(size, {
+    TEST(sizeof(padded_file_header_t) == PAGE_SIZE);
+    TEST(sizeof(page_header_t) == 128);
+    TEST(sizeof(padded_free_page_t) == 128);
+    TEST(sizeof(record_t) == 128);
+    TEST(sizeof(page_t) == PAGE_SIZE);
+})
 
+TEST_SUITE(headers, {
+    return size_test();
 })
