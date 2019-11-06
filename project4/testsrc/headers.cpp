ac3278309e9d1e284186941795adaaad2c50f9f8
revsic <revsic99@gmail.com>
2019-11-06 15:50:34 +0900

    headers: Rename test name

--- project4/testsrc/headers.cpp
+++ project4/testsrc/headers.cpp
@@ -12,16 +12,16 @@ TEST_SUITE(page_init, {
     // TODO: Impl test
 })
 
-TEST_SUITE(page_release, {
+TEST_SUITE(page_release_abstraction, {
     // TODO: Impl test
 })
 
-TEST_SUITE(page_create, {
+TEST_SUITE(page_create_abstraction, {
     // TODO: Impl test
 
 })
 
-TEST_SUITE(page_extend_free, {
+TEST_SUITE(page_extend_free_abstraction, {
     // TODO: Impl test
 
 })
@@ -35,8 +35,8 @@ TEST_SUITE(headers, {
     static_assert(sizeof(Page) == PAGE_SIZE, "the size of Page is not PAGE_SIZE");
     return size_test()
         && page_init_test()
-        && page_release_test()
-        && page_create_test()
-        && page_extend_free_test()
+        && page_release_abstraction_test()
+        && page_create_abstraction_test()
+        && page_extend_free_abstraction_test()
         && page_getter_test();
 })
