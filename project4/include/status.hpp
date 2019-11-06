5d98d0a81d49e26debec395f91c27cf5b7a0b155
revsic <revsic99@gmail.com>
2019-11-06 12:36:01 +0900

    status: Add exit on false

--- project4/include/status.hpp
+++ project4/include/status.hpp
@@ -13,6 +13,9 @@ enum class status_t { SUCCESS = 0, FAILURE = 1 };
 /// Return FAILURE if given is false or 0.
 #define CHECK_TRUE(x) if (!(x)) { return status_t::FAILURE; }
 
+/// Exit process if given is FALSE.
+#define EXIT_ON_FALSE(x) if (!(x)) { printf("check failure: file %s, line %d\n", __FILE__, __LINE__); exit(1); }
+
 /// Return FAILURE if given is NULL.
 #define CHECK_NULL(x) if ((x) == NULL) { return status_t::FAILURE; }
 
