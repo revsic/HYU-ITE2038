20dfb3029c3ada6a6dafd96f7f6a83c870f84f68
revsic <revsic99@gmail.com>
2019-11-12 15:36:53 +0900

    main: Impl simple console

--- project4/app/main.cpp
+++ project4/app/main.cpp
@@ -1,5 +1,81 @@
-// MAIN
+#include <iostream>
+
+#include "buffer_manager.hpp"
+#include "table_manager.hpp"
 
 int main(int argc, char* argv[]) {
+    TableManager tables;
+    BufferManager buffers(4);
+
+    tableid_t tid = tables.load("datafile", buffers);
+    Table* table = tables.find(tid);
+
+    bool runnable = true;
+    while (!std::cin.eof() && runnable) {
+        char inst;
+        int input, range;
+        Record record;
+        std::string value;
+
+        std::cout << '>';
+        std::cin >> inst;
+        switch(inst) {
+        case 'o':
+            std::cin >> value;
+            tables.remove(tid);
+            tid = tables.load(value, buffers);
+            table = tables.find(tid);
+            break;
+        case 'd':
+            std::cin >> input;
+            table->remove(input);
+            table->print_tree();
+            break;
+        case 'i':
+            std::cin >> input;
+            value = std::to_string(input) + " value";
+            table->insert(
+                input,
+                reinterpret_cast<const uint8_t*>(value.c_str()),
+                value.size());
+            table->print_tree();
+            break;
+        case 'f':
+            std::cin >> input;
+            if (table->find(input, &record) == Status::SUCCESS) {
+                std::cout
+                    << "Key: " << input << ' '
+                    << "Value: " << record.value << std::endl;
+            } else {
+                std::cout << "not found" << std::endl;
+            }
+            break;
+        case 'r':
+            std::cin >> input >> range;
+            if (input > range) {
+                std::swap(input, range);
+            }
+            for (Record const& rec : table->find_range(input, range)) {
+                std::cout
+                    << "Key: " << rec.key << ' '
+                    << "Value: " << rec.value << std::endl;
+            }
+            break;
+        case 'q':
+            runnable = false;
+            break;
+        case 't':
+            table->print_tree();
+            break;
+        case 'x':
+            table->destroy_tree();
+            table->print_tree();
+            break;
+        default:
+            break;
+        }
+        while (getchar() != '\n');
+    }
+
     return 0;
 }
