6606c8ab670a8cd453bd600b617df3a75dd75de6
revsic <revsic99@gmail.com>
2019-11-13 20:11:39 +0900

    dbapi: Add db api

--- project4/include/dbapi.hpp
+++ project4/include/dbapi.hpp
@@ -1,6 +1,22 @@
 #ifndef DBAPI_HPP
 #define DBAPI_HPP
 
+#include "dbms.hpp"
 
+extern Database GLOBAL_DB;
+
+int init_db(int buf_num);
+
+int open_table(char const* pathname);
+
+int db_insert(int table_id, int64_t key, char const* value);
+
+int db_find(int table_id, int64_t key, char* ret_val);
+
+int db_delete(int table_id, int64_t key);
+
+int close_table(int table_id);
+
+int shutdown_db();
 
 #endif
