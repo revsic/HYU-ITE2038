bc0463bd03c2c5a02d91e265a10273307e1e453b
revsic <revsic99@gmail.com>
2019-11-14 03:48:00 +0900

    join test: Add big size test

--- project4/testsrc/join_test.cpp
+++ project4/testsrc/join_test.cpp
@@ -1,11 +1,11 @@
 #include "dbms.hpp"
 #include "join.hpp"
 
-int arr1[] = { 1, 2, 3, 5, 7};
-int arr2[] = { 1, 4, 5, 6, 7};
+int arr1[] = { 1, 2, 3, 5, 7 };
+int arr2[] = { 1, 4, 5, 6, 7 };
 
 TEST_SUITE(set_merge, {
-    Database dbms(5);
+    Database dbms(100000);
 
     tableid_t tid1 = dbms.open_table("testfile1");
     tableid_t tid2 = dbms.open_table("testfile2");
@@ -28,6 +28,29 @@ TEST_SUITE(set_merge, {
     TEST(found[0] == 1);
     TEST(found[1] == 5);
     TEST(found[2] == 7);
+
+    dbms.destroy_tree(tid1);
+    dbms.destroy_tree(tid2);
+
+    constexpr int num_insert =
+        BPTree::DEFAULT_LEAF_ORDER * BPTree::DEFAULT_INTERNAL_ORDER / 4;
+    for (int i = 0; i < num_insert; ++i) {
+        dbms.insert(tid1, i * 2, arr, 5);
+        dbms.insert(tid2, i * 3, arr, 5);
+        TEST_SUCCESS(dbms.find(tid1, i * 2, nullptr));
+        TEST_SUCCESS(dbms.find(tid2, i * 3, nullptr));
+    }
+
+    found.clear();
+    JoinOper::set_merge(dbms[tid1], dbms[tid2],
+        [&](Record& rec1, Record&) {
+            found.push_back(rec1.key);
+            return Status::SUCCESS;
+        });
+
+    for (int num : found) {
+        TEST(num % 6 == 0);
+    }
 
     dbms.~Database();
     remove("testfile1");
