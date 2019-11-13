8fed1584022edcd654d82c90e275962105a1b091
revsic <revsic99@gmail.com>
2019-11-13 15:56:23 +0900

    dbms: Add annotation

--- project4/include/dbms.hpp
+++ project4/include/dbms.hpp
@@ -4,26 +4,48 @@
 #include "buffer_manager.hpp"
 #include "table_manager.hpp"
 
+/// Database management system.
 class Database {
 public:
+    /// Construct database with the number of buffers.
+    /// \param num_buffer int, number of buffers.
     Database(int num_buffer);
 
+    /// Default destructor.
     ~Database() = default;
 
+    /// Deleted copy constructor.
     Database(Database const&) = delete;
 
+    /// Deleted move constructor.
     Database(Database&&) = delete;
 
+    /// Deleted copy assignment.
     Database& operator=(Database const&) = delete;
 
+    /// Deleted move assignment.
     Database& operator=(Database&&) = delete;
 
+    /// Open table with given filename.
+    /// \param filename std::string const&, the name of the file.
+    /// \return tableid_t, created table ID.
     tableid_t open_table(std::string const& filename);
 
+    /// Close table.
+    /// \param id tableid_t, table ID.
+    /// \return Status, whether success to close table or not.
     Status close_table(tableid_t id);
 
+    /// Print tree.
+    /// \param id tableid_t, table ID.
+    /// \return Status, whether success to print tree or not.
     Status print_tree(tableid_t id);
 
+    /// Find the record with given key and write the result to record pointer.
+    /// \param id tableid_t, table ID.
+    /// \param key prikey_t, primary key.
+    /// \param record Record*, record pointer to write the result.
+    /// \return Status, whether success to find the record or not.
     Status find(tableid_t id, prikey_t key, Record* record);
 
     std::vector<Record> find_range(tableid_t, prikey_t start, prikey_t end);
