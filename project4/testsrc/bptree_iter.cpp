0185eca17724aa89ef7791c76c41a37a547383e9
revsic <revsic99@gmail.com>
2019-11-14 11:43:56 +0900

    bptree iter: Add b+tree iterator tests

--- project4/testsrc/bptree_iter.cpp
+++ project4/testsrc/bptree_iter.cpp
@@ -1,5 +1,49 @@
 #include "bptree_iter.hpp"
 
+TEST_SUITE(copy_ctor, {
+
+})
+
+TEST_SUITE(copy_assign, {
+
+})
+
+TEST_SUITE(move_ctor, {
+
+})
+
+TEST_SUITE(move_assign, {
+
+})
+
+TEST_SUITE(begin, {
+
+})
+
+TEST_SUITE(end, {
+
+})
+
+TEST_SUITE(inc_operator, {
+
+})
+
+TEST_SUITE(cmp_operator, {
+
+})
+
+TEST_SUITE(ref_operator, {
+
+})
+
 int bptree_iter_test() {
-    return 1;
+    return copy_ctor_test()
+        && copy_assign_test()
+        && move_ctor_test()
+        && move_assign_test()
+        && begin_test()
+        && end_test()
+        && inc_operator_test()
+        && cmp_operator_test()
+        && ref_operator_test();
 }
