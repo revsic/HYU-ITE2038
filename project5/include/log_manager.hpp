d20b13deae18b6f7e55e7597e19bf5df848918f8
revsic <revsic99@gmail.com>
2019-12-04 01:09:07 +0900

    log manager: Add annotations

--- project5/include/log_manager.hpp
+++ project5/include/log_manager.hpp
@@ -7,6 +7,7 @@
 #include "headers.hpp"
 #include "lock_manager.hpp"
 
+/// Log types.
 enum class LogType {
     INVALID = 0,
     UPDATE = 1,
@@ -16,49 +17,95 @@ enum class LogType {
     END = 5,
 };
 
+/// Log structure.
 struct Log {
+    /// Default constructor.
     Log();
-
+    /// Constructor for recordless log.
+    /// \param lsn lsn_t, log sequence number.
+    /// \param prev_lsn lsn_t, previous lsn.
+    /// \param xid trxid_t, transaction ID.
+    /// \param type LogType, log type.
     Log(lsn_t lsn, lsn_t prev_lsn, trxid_t xid, LogType type);
 
+    /// Constructor for record relative log.
+    /// \param lsn lsn_t, log sequence number.
+    /// \param prev_lsn lsn_t, previous lsn.
+    /// \param xid trxid_t, transaction ID.
+    /// \param type LogType, log type.
+    /// \param hid HID, hierarchical ID.
+    /// \param offset int, record offset in target page.
+    /// \param before Record const&, before record.
+    /// \param after Record const&, after record.
     Log(lsn_t lsn, lsn_t prev_lsn, trxid_t xid, LogType type,
         HID hid, int offset, Record const& before, Record const& after);
 
-    lsn_t lsn;
-    lsn_t prev_lsn;
-    trxid_t xid;
-    LogType type;
-    HID hid;
-    int offset;
-    Record before;
-    Record after;
+    lsn_t lsn;          /// log sequence number.
+    lsn_t prev_lsn;     /// previous lsn.
+    trxid_t xid;        /// transaction ID.
+    LogType type;       /// log type.
+    HID hid;            /// hierarchical ID.
+    int offset;         /// record offset in target page.
+    Record before;      /// before record.
+    Record after;       /// after record.
 };
 
+
+/// Log manager.
 class LogManager {
 public:
+    /// Default constructor.
     LogManager();
 
+    /// Write log about record update.
+    /// \param xid trxid_t, transaction ID.
+    /// \param hid HID, hierarchical ID.
+    /// \param offset int, record index.
+    /// \param before Record const&, before record.
+    /// \param after Record const&, after record.
+    /// \return lsn_t, allocated log sequence number.
     lsn_t log_update(
         trxid_t xid, HID hid, int offset,
         Record const& before, Record const& after);
     
+    /// Write log about transaction abort.
+    /// \param xid trxid_t, transaction ID.
+    /// \return lsn_t, allocated log sequence number.
     lsn_t log_abort(trxid_t xid);
 
+    /// Write log about commited transaction.
+    /// \param xid trxid_t, transaction ID.
+    /// \return lsn_t, allocated log sequence number.
     lsn_t log_commit(trxid_t xid);
 
+    /// Write log about end transaction.
+    /// \param xid trxid_t, transaction ID.
+    /// \return lsn_t, allocated log sequence number.
     lsn_t log_end(trxid_t xid);
 
+    /// Get all logs about given transaction ID.
+    /// \return std::list<Log> const&, log list in reverse chronological order.
     std::list<Log> const& get_logs(trxid_t xid) const;
 
+    /// Remove transaction log about given transaction ID.
+    /// \param xid trxid_t, transaction ID.
+    /// \return Status, whether success to remove all logs about xid or not.
     Status remove_trxlog(trxid_t xid);
 
 private:
-    std::recursive_mutex mtx;
-    lsn_t last_lsn;
-    std::map<trxid_t, std::list<Log>> log_map;
+    std::recursive_mutex mtx;                       /// System level mutex.
+    lsn_t last_lsn;                                 /// last log sequence number.
+    std::map<trxid_t, std::list<Log>> log_map;      /// logs.
 
+    /// Get possible log sequence number.
+    /// \return lsn_t, monotonically increasing lsn.
     lsn_t get_lsn();
 
+    /// Log writer.
+    /// \tparam typename... Args, arguments type.
+    /// \param xid trxid_t, transaction ID.
+    /// \param args Args&&..., arguments for log constructor.
+    /// \return lsn_t, allocated log sequence number.
     template <typename... Args>
     lsn_t wrapper(trxid_t xid, Args&&... args) {
         std::unique_lock<std::recursive_mutex> own(mtx);
