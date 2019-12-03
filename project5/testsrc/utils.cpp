fdec9c1b0b9f974351de3750ac20a369debc1a2b
revsic <revsic99@gmail.com>
2019-12-04 01:37:38 +0900

    utils: Add defer test

--- project5/testsrc/utils.cpp
+++ project5/testsrc/utils.cpp
@@ -1,5 +1,16 @@
+#include "utils.hpp"
 #include "test.hpp"
 
+TEST_SUITE(defer, {
+    bool flag = false;
+    {
+        auto defer = utils::defer([&] {
+            flag = true;
+        });
+    }
+    TEST(flag);
+})
+
 int utils_test() {
-    return 1;
+    return defer_test();
 }
