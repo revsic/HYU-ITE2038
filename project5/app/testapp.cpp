e4c5455e0947078bc9b2f44214116751f59e36c6
revsic <revsic99@gmail.com>
2019-12-05 05:48:19 +0900

    testapp: Add testapp

--- project5/app/testapp.cpp
+++ project5/app/testapp.cpp
@@ -1,28 +1,61 @@
+#include <atomic>
+#include <cstring>
 #include <chrono>
-#include <future>
+#include <random>
+#include <thread>
 
 #include "dbms.hpp"
 
-#define TEST(expr) if(!(expr)) { printf("%s, line %d: err\n", __FILE__, __LINE__); return 0; }
-#define TEST_STATUS(expr) if(!(expr)) { printf("%s, line %d: err\n", __FILE__, __LINE__); return Status::FAILURE; }
-#define TEST_SUCCESS(val) TEST(val == Status::SUCCESS);
-
-using namespace std::chrono_literals;
-
 int main() {
-    Database dbms(4);
-    LockManager& manager = dbms.locks;
+    Database dbms(100000);
+    tableid_t tid = dbms.open_table("database1.db");
 
-    trxid_t xid = dbms.begin_trx();
-    trxid_t xid2 = dbms.begin_trx();
-    TEST_SUCCESS(dbms.trxs.require_lock(xid, HID(1, 2), LockMode::EXCLUSIVE));
-    TEST_SUCCESS(dbms.trxs.require_lock(xid2, HID(2, 1), LockMode::EXCLUSIVE));
+    constexpr size_t NUM_THREAD = 4;
+    constexpr size_t MAX_TRX = 1000;
+    constexpr size_t MAX_TRXSEQ = 1000;
 
-    Status res;
-    std::thread thread([&] {
-        res = dbms.trxs.require_lock(xid, HID(2, 1), LockMode::EXCLUSIVE);
-        return 0;
+    std::atomic<int> n_query(0);
+
+    std::thread threads[NUM_THREAD];
+    for (int i = 0; i < NUM_THREAD; ++i) {
+        threads[i] = std::thread([&] {
+            std::random_device rd;
+            std::default_random_engine gen(rd());
+
+            int num_trx = gen() % MAX_TRX;
+            for (int j = 0; j < num_trx; ++j) {
+                trxid_t xid = dbms.begin_trx();
+
+                int num_seq = gen() % MAX_TRXSEQ;
+                for (int k = 0; k < num_seq; ++k) {
+                    ++n_query;
+                    prikey_t key = gen() % 100000;
+                    if (gen() % 2 == 0) {
+                        dbms.find(tid, key, nullptr, xid);
+                    } else {
+                        Record record;
+                        std::strncpy(reinterpret_cast<char*>(record.value), "hello", 10);
+                        dbms.update(tid, key, record, xid);
+                    }
+                }
+            }
+        });
+    }
+
+    bool runnable = true;
+    std::thread logger([&] {
+        while (runnable) {
+            std::printf("\r%d", n_query.load());
+        }
     });
 
-    Status res2 = dbms.trxs.require_lock(xid2, HID(1, 2), LockMode::EXCLUSIVE);
+    for (int i = 0; i < NUM_THREAD; ++i) {
+        threads[i].join();
+    }
+    runnable = false;
+    logger.join();
+
+    std::printf("\n");
+
+    return 0;
 }
